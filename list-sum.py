787d669bee0fa6f280d1008c43ba9c5a96f14852
alibinfaizan555 <60197613+alibinfaizan555@users.noreply.github.com>
2020-08-03 16:33:13 +0530

    list-sum.py
    Sum of all the elements in a list.

---
+++ list-sum.py
@@ -0,0 +1,5 @@
+mylist = [2, 6, 8, 3, 4, 76, 3] #or you can get the input from the user
+sum = 0
+for i in mylist: #iterate through the list
+    sum += i
+print(sum) #prints the sum of all the elements in mylist
